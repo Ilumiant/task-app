98b68950e39917f1b6aadccf44ce5d75cfb56bcc
Antonio Alvarez <antonio@ilumiware.com>
2020-09-05 18:01:29 -0500

    get tasks

---
+++ src/components/Tasks.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react'
+import Axios from 'axios'
+function Tasks() {
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    Axios.get('http://task.test/api/v1/tasks').then(response => setTasks(response.data))
+  },[])
+
+  return (
+    <div>
+      {tasks.map(task => (
+        <div>
+          <div>
+            <span>{task.id}.- {task.name}</span>
+            <input type="checkbox" value="realizado" checked={task.done} />
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Tasks
